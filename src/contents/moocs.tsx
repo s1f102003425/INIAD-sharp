50ea6d61f0d86cabbd8df0f69e4814bd120feac7
s1f102003425 <s1f102003425@iniad.org>
2023-10-12 22:00:42 +0900

    add ui

--- src/contents/moocs.tsx
+++ src/contents/moocs.tsx
@@ -1,12 +1,41 @@
-import axios from "axios"
+// import axios from "axios"
+import type { PlasmoCSConfig, PlasmoGetInlineAnchor } from "plasmo"
+import type { CSSProperties } from "react"
+import React, { useEffect, useState } from "react"
 
-const url = `https://raw.githubusercontent.com/yuon7/deadline-json/main/data.json`
+// const url = `https://raw.githubusercontent.com/yuon7/deadline-json/main/data.json`
 
-const fetchFileContent = async () => {
-  const response = await axios.get(url)
-  const data = response.data
-  console.log(data)
-  console.log(data.cs2[0].内容)
+// const fetchFileContent = async () => {
+//   const response = await axios.get(url)
+//   const data = response.data
+//   console.log(data)
+//   console.log(data.cs2[0].内容)
+// }
+
+// fetchFileContent()
+
+export const config: PlasmoCSConfig = {
+  matches: ["https://moocs.iniad.org/*"]
 }
 
-fetchFileContent()
+export const getInlineAnchor: PlasmoGetInlineAnchor = async () => {
+  const aside = document
+    .querySelector("aside")
+    ?.querySelector("section")
+    ?.querySelector("ul > li:last-child") as Element
+  return aside
+}
+
+export const getShadowHostId = () => "INIAD-sharp"
+
+const styles: CSSProperties = {
+  boxSizing: "border-box",
+  fontWeight: "bold",
+  color: "#fff",
+  fontSize: "0.7rem"
+}
+const PlasmoInline = () => {
+  return <div style={styles}>aaaaaaaaaaaaa</div>
+}
+
+export default PlasmoInline
