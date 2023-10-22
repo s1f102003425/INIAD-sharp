856be086e6d8a339aa52d529cd483b8d94581506
s1f10230014 <s1f102300144@iniad.org>
2023-10-23 04:44:44 +0900

    chore:全url取得機能追加

--- src/contents/slide.tsx
+++ src/contents/slide.tsx
@@ -1,40 +1,49 @@
 import axios from "axios"
-import { useCallback, useEffect, useState } from "react"
 
-const DisplayData = () => {
-  const [changedData, setChangedData] = useState<string>()
+const urls = []
 
-  const getSlideData = (): Promise<string> => {
-    return axios
-      .get(
-        "https://raw.githubusercontent.com/jun-eg/test-zip/main/data/slide.json"
-      )
-      .then((response) => {
-        return response.data
-      })
-      .catch((error) => {
-        console.error(error)
-      })
-  }
-
-  const fetchData = useCallback(async () => {
-    try {
-      const data = await getSlideData()
-      setChangedData(data)
-    } catch (error) {
+const getSlideData = (): Promise<string> => {
+  return axios
+    .get(
+      "https://raw.githubusercontent.com/jun-eg/test-zip/main/data/slide.json"
+    )
+    .then((response) => {
+      return response.data
+    })
+    .catch((error) => {
       console.error(error)
-    }
-  }, [])
-
-  useEffect(() => {
-    fetchData()
-  }, [fetchData])
-
-  return (
-    <div className="sidebar-menu tree">
-      <li>{JSON.stringify(changedData)}</li>
-    </div>
-  )
+    })
 }
 
-export default DisplayData
+type NestedObject = {
+  [key: string]: string | NestedObject
+}
+
+const isNestedObject = (value: unknown): value is NestedObject => {
+  return typeof value === "object" && value !== null
+}
+
+const findUrls = (obj: unknown) => {
+  if (isNestedObject(obj)) {
+    for (const key in obj) {
+      const value = obj[key]
+      if (isNestedObject(value)) {
+        findUrls(value)
+      } else if (key === "url" && typeof value === "string") {
+        urls.push(value)
+      }
+    }
+  }
+  console.log(urls)
+}
+
+const fetchData = async () => {
+  try {
+    const data = await getSlideData()
+    findUrls(data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+fetchData()
