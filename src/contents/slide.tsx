d13a41e17baafd0a93d67b9915d2e676d1e76efb
s1f10230014 <s1f102300144@iniad.org>
2023-10-23 02:36:07 +0900

    chore:moocs上にjsonデータ全表示

--- src/contents/slide.tsx
+++ src/contents/slide.tsx
@@ -1,16 +1,40 @@
 import axios from "axios"
+import { useCallback, useEffect, useState } from "react"
 
-const getSlideData = () => {
-  axios
-    .get(
-      "https://raw.githubusercontent.com/jun-eg/test-zip/main/data/slide.json"
-    )
-    .then((response) => {
-      console.log(response.data)
-    })
-    .catch((error) => {
+const DisplayData = () => {
+  const [changedData, setChangedData] = useState<string>()
+
+  const getSlideData = (): Promise<string> => {
+    return axios
+      .get(
+        "https://raw.githubusercontent.com/jun-eg/test-zip/main/data/slide.json"
+      )
+      .then((response) => {
+        return response.data
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
+  const fetchData = useCallback(async () => {
+    try {
+      const data = await getSlideData()
+      setChangedData(data)
+    } catch (error) {
       console.error(error)
-    })
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
+  return (
+    <div className="sidebar-menu tree">
+      <li>{JSON.stringify(changedData)}</li>
+    </div>
+  )
 }
 
-getSlideData()
+export default DisplayData
