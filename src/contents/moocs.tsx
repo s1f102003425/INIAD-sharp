6a3b10729dc4670437a9e2da34fa416eb5e16b96
s1f10230014 <s1f102300144@iniad.org>
2023-10-23 16:37:08 +0900

    一時的にtestdata追加

--- src/contents/moocs.tsx
+++ src/contents/moocs.tsx
@@ -1,12 +1,28 @@
 import axios from "axios"
+import { useCallback, useEffect, useState } from "react"
+import type { Assignment, Data } from "src/types/moocsTypes"
 
-const url = `https://raw.githubusercontent.com/yuon7/deadline-json/main/data.json`
+const url = `https://raw.githubusercontent.com/jun-eg/deadline-json-fork/main/data.json`
 
-const fetchFileContent = async () => {
-  const response = await axios.get(url)
-  const data = response.data
-  console.log(data)
-  console.log(data.cs2[0].内容)
+const FetchDeadLineData = () => {
+  const [data, setData] = useState<Data | null>(null)
+
+  const getFiledata = useCallback(async () => {
+    try {
+      const response = await axios.get<typeof data>(url)
+      setData(response.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }, [])
+
+  useEffect(() => {
+    getFiledata()
+  }, [getFiledata])
+
+  if (!data) return <div>GettingData</div>
+
+  return <div>{}</div>
 }
 
-fetchFileContent()
+export default FetchDeadLineData
